dc99c892bbd1f5da0b6385d44fb038c6183242df
Mdiftekhar9123 <md.iftekhar@successive.tech>
2021-11-18 19:37:48 +0530

    Added configuration.js and index.js and server.js and modified the files

--- Extra/index.js
+++ Extra/index.js
@@ -1,15 +1,15 @@
 import { diamond,equilateralTriangle,pyramid } from "./pattern";
 
 equilateralTriangle(5);
-diamond(5);
-pyramid('fullPyramid', 5);
+diamond(7);
+pyramid('halfPyramid', 5);
 
-import { hasPermission } from  "./utils";
+// import { hasPermission } from  "./utils";
 
-let Obj = hasPermission( 'getUsers','trainee','read' );
-console.log("Ans:",Obj);
+// let Obj = hasPermission( 'getUsers','trainee','read' );
+// console.log("Ans:",Obj);
 
-import { validateUser } from "./utils";
+// import { validateUser } from "./utils";
 
-const users = ['trainee1@successive.tech','reviewer1@successive.tech','mdiftekhar841@gmail.com','xyz1gmail.com','demo123gamil.co.in'];
-validateUser(users);
+// const users = ['trainee1@successive.tech','reviewer1@successive.tech','mdiftekhar841@gmail.com','xyz1gmail.com','demo123gamil.co.in'];
+// validateUser(users);
--- Extra/pattern/index.js
+++ Extra/pattern/index.js
@@ -1,3 +1,4 @@
 export {default as diamond} from './diamond'
 export {default as equilateralTriangle} from './equilateral'
 export {default as pyramid} from './pyramid'
+
